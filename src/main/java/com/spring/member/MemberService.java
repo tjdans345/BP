59c533cee3707b7574c2043fe50f314f539946ff
moon <tjdans345@naver.com>
2020-08-26 14:38:43 +0900

    ..

--- src/main/java/com/spring/member/MemberService.java
+++ src/main/java/com/spring/member/MemberService.java
@@ -15,15 +15,10 @@ public class MemberService {
 	public String loginMember(MemberVO memberVO) {
 		
 		String msg = "";
-<<<<<<< HEAD
-		List list = mdao.idCheck(memberVO.getId());
-		if(list.size() == 0) {
-			msg="아이디를 입력하세요";
-=======
+
 		List list = mdao.idCheck(memberVO.getId()); 
 		if(list.size()==0) {
 			msg="아이디를 확인해주세요";
->>>>>>> da0298bc38d2ef1e0f96959560024922c1571e09
 		}else {
 			MemberVO VO = mdao.loginMember(memberVO.getId());
 			if(memberVO.getPassword().equals(VO.getPassword())) {
