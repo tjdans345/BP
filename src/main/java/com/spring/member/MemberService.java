eaee8047e7558d5f229acb4e7369cd304590ed77
moon <tjdans345@naver.com>
2020-08-26 14:15:48 +0900

    ..

--- src/main/java/com/spring/member/MemberService.java
+++ src/main/java/com/spring/member/MemberService.java
@@ -14,9 +14,10 @@ public class MemberService {
 	}
 	
 	public String loginMember(MemberVO memberVO) {
+		
 		String msg = "";
-		List list = mdao.idCheck(memberVO.getId()); 
-		if(list.size()==0) {
+		List list = mdao.idCheck(memberVO.getId());
+		if(list.size() == 0) {
 			msg="아이디를 입력하세요";
 		}else {
 			MemberVO VO = mdao.loginMember(memberVO.getId());
@@ -26,6 +27,7 @@ public class MemberService {
 				msg = "비밀번호를 확인해주세요";
 			}
 		}
+		
 		return msg;
 	}
 		
